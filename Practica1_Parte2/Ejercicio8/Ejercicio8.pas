ecbd33091e92b1550983965f7893eb6bff4b04ac
Tomgamma <104223036+tomgamma@users.noreply.github.com>
2024-04-17 11:04:23 -0300

    Update Ejercicio8.pas

--- Practica1_Parte2/Ejercicio8/Ejercicio8.pas
+++ Practica1_Parte2/Ejercicio8/Ejercicio8.pas
@@ -1,3 +1,6 @@
+// Exercise: "Un local de ropa desea analizar las ventas realizadas en el último mes. Para ello se lee por cada día del mes, los montos de las ventas realizadas. La lectura de montos para cada día finaliza cuando se lee el monto 0. Se asume un mes de 31 días. Informar la cantidad de ventas por cada día, y el monto total acumulado en ventas de todo el mes.
+//              a. Modifique el ejercicio anterior para que además informe el día en el que se realizó la mayor cantidad de ventas"
+
 program Ejercicio8;
 
 const
