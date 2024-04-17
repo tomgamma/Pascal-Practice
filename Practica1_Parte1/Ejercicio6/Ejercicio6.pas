4aa7692c273341cae02f37eb6d03fb1fb5a21c34
Tomgamma <104223036+tomgamma@users.noreply.github.com>
2024-04-16 22:15:37 -0300

    Post the question of the exercise and fix errors in the comments

--- Practica1_Parte1/Ejercicio6/Ejercicio6.pas
+++ Practica1_Parte1/Ejercicio6/Ejercicio6.pas
@@ -1,3 +1,16 @@
+// Exercise: "Realizar un programa que lea el número de legajo y el promedio de cada alumno de la facultad. La lectura finaliza cuando se ingresa el legajo -1, que no debe procesarse. 
+//              Por ejemplo: se lee la siguiente secuencia:
+//                33423
+//                8.40
+//                19003
+//                6.43
+//                -1
+//              En el ejemplo anterior, se leyó el legajo 33422, cuyo promedio fue 8.40, luego se leyó el legajo 19003, cuyo promedio fue 6.43, y finalmente el legajo -1 (para el cual no es necesario leer un promedio).
+//              Al finalizar la lectura, informar:
+//                a. La cantidad de alumnos leída (en el ejemplo anterior, se debería informar 2).
+//                b. La cantidad de alumnos cuyo promedio supera 6.5 (en el ejemplo anterior, se debería informar 1).
+//                c. El porcentaje de alumnos destacados (alumnos con promedio mayor a 8.5) cuyo legajo sean menor al valor 2500 (en el ejemplo anterior, se debería informar 0%)."
+
 program Ejercicio6;
 
 var
