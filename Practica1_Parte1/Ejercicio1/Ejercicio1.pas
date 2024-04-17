769ebc835941277c4a72f066a40ddbd4052e06ba
Tomgamma <104223036+tomgamma@users.noreply.github.com>
2024-04-16 22:00:16 -0300

    Post the question of the exercise and fix errors in the comments

--- Practica1_Parte1/Ejercicio1/Ejercicio1.pas
+++ Practica1_Parte1/Ejercicio1/Ejercicio1.pas
@@ -1,3 +1,5 @@
+// Exercise: "Realizar un programa que lea 2 números enteros desde teclado e informe en pantalla cuál de los dos números es el mayor. Si son iguales debe informar en pantalla lo siguiente: Los números leídos son iguales"
+
 program Ejercicio1;
 
 var
