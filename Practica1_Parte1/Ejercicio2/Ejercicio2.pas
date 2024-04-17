a480e2ce3e509da0df141d570f2e58ce968f8bc9
Tomgamma <104223036+tomgamma@users.noreply.github.com>
2024-04-16 22:03:19 -0300

    Post the question of the exercise and fix errors in the comments

--- Practica1_Parte1/Ejercicio2/Ejercicio2.pas
+++ Practica1_Parte1/Ejercicio2/Ejercicio2.pas
@@ -1,3 +1,7 @@
+// Exercise: "Realizar un programa que lea un número real e imprima su valor absoluto. El valor absoluto de un número X, se escribe |X| y se define como:
+//            |X| = X cuando X es mayor o igual a cero
+//            |X| = -X cuando X es menor a cero
+
 program Ejercicio2;
 
 var
