f1540905f1a37b895c47580291ad64de70629a8c
Tomgamma <104223036+tomgamma@users.noreply.github.com>
2024-04-17 10:53:30 -0300

    Update Ejercicio2.pas

--- Practica1_Parte2/Ejercicio2/Ejercicio2.pas
+++ Practica1_Parte2/Ejercicio2/Ejercicio2.pas
@@ -1,3 +1,5 @@
+// Exercise: "Realice un programa que lea 10 números e informe cuál fue el mayor número leído. Por ejemplo: si se lee la secuencia: 3 5 6 2 3 10 98 8 -12 9, deberá informar: El mayor número leído fue el 98"
+
 program Ejercicio2;
 
 var
