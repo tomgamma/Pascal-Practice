b3efacd98228fe01fb2296ab407ef9cab2c15ee3
Tomgamma <104223036+tomgamma@users.noreply.github.com>
2024-04-17 11:00:46 -0300

    Update Ejercicio4.pas

--- Practica1_Parte2/Ejercicio4/Ejercicio4.pas
+++ Practica1_Parte2/Ejercicio4/Ejercicio4.pas
@@ -1,3 +1,7 @@
+// Exercise: "Realizar un programa que lea 1000 números enteros desde teclado. Informar en pantalla cuáles son los dos números mínimos leídos.
+//              a. Modifique el ejercicio anterior para que, en vez de leer 1000 números, la lectura finalice al leer el número 0, el cual debe procesarse.
+//              b. Modifique el ejercicio anterior para que, en vez de leer 1000 números, la lectura finalice al leer el número 0, el cual no debe procesarse"
+
 program Ejercicio4;
 
 var
