992c1bc79471a954bd9257dcb4cfa2e9e4162136
Tomgamma <104223036+tomgamma@users.noreply.github.com>
2024-04-17 11:02:29 -0300

    Update Ejercicio6.pas

--- Practica1_Parte2/Ejercicio6/Ejercicio6.pas
+++ Practica1_Parte2/Ejercicio6/Ejercicio6.pas
@@ -1,3 +1,7 @@
+// Exercise: "Realizar un programa que lea información de 200 productos de un supermercado. De cada producto se lee código y precio (cada código es un número entre 1 y 200). Informar en pantalla:
+//              ● Los códigos de los dos productos más baratos.
+//              ● La cantidad de productos de más de 16 pesos con código par"
+
 program Ejercicio6;
 
 var
