d2e70b175399bd53d8ae7a553480a1939a6df834
Tomgamma <104223036+tomgamma@users.noreply.github.com>
2024-04-16 22:05:00 -0300

    Post the question of the exercise and fix errors in the comments

--- Practica1_Parte1/Ejercicio3/Ejercicio3.pas
+++ Practica1_Parte1/Ejercicio3/Ejercicio3.pas
@@ -1,3 +1,5 @@
+// Exercise: "Realizar un programa que lea 3 números enteros y los imprima en orden descendente. Por ejemplo: si se ingresan los valores 4, -10 y 12, deberá imprimir: 12 4 -10"
+
 program Ejercicio3;
 
 var
