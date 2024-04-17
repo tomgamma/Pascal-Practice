ed5e243a5caf6cb917b91de6dbb3c4fd22969a85
Tomgamma <104223036+tomgamma@users.noreply.github.com>
2024-04-17 11:03:22 -0300

    Update Ejercicio7.pas

--- Practica1_Parte2/Ejercicio7/Ejercicio7.pas
+++ Practica1_Parte2/Ejercicio7/Ejercicio7.pas
@@ -1,3 +1,7 @@
+// Exercise: "Realizar un programa que lea desde teclado información de autos de carrera. Para cada uno de los autos se lee el nombre del piloto y el tiempo total que le tomó finalizar la carrera. En la carrera participaron 100 autos. Informar en pantalla:
+//              ● Los nombres de los dos pilotos que finalizaron en los dos primeros puestos.
+//              ● Los nombres de los dos pilotos que finalizaron en los dos últimos puestos."
+
 program Ejercicio7;
 
 var
