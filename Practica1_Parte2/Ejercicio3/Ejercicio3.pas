714805c12d7a41e8717aaab3f51cc60959139bbb
Tomgamma <104223036+tomgamma@users.noreply.github.com>
2024-04-17 10:59:45 -0300

    Update Ejercicio3.pas

--- Practica1_Parte2/Ejercicio3/Ejercicio3.pas
+++ Practica1_Parte2/Ejercicio3/Ejercicio3.pas
@@ -1,3 +1,8 @@
+// Exercise: "Realizar un programa que lea desde teclado la información de alumnos ingresantes a la carrera Analista en TIC. De cada alumno se lee nombre y nota obtenida en el módulo EPA (la nota es un número entre 1 y 10).
+//              La lectura finaliza cuando se lee el nombre “Zidane Zinedine“, que debe procesarse. Al finalizar la lectura informar:
+//                ● La cantidad de alumnos aprobados (nota 8 o mayor).
+//                ● La cantidad de alumnos que obtuvieron un 7 como nota"
+
 program Ejercicio3;
 
 var
