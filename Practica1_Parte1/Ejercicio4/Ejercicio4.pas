f52eb449ce11a3eac0bcd918f0804e362bc5b961
Tomgamma <104223036+tomgamma@users.noreply.github.com>
2024-04-16 22:07:50 -0300

    Post the question of the exercise and fix errors in the comments

--- Practica1_Parte1/Ejercicio4/Ejercicio4.pas
+++ Practica1_Parte1/Ejercicio4/Ejercicio4.pas
@@ -1,6 +1,6 @@
 // Exercise: "Realizar un programa que lea un número real X. Luego, deberá leer números reales hasta que se ingrese uno cuyo valor sea exactamente el doble de X (el primer número leído)."
 
-program BusquedaDoble;
+program Ejercicio4;
 
 var
   X, numero: real;
